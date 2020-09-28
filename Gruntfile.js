2f89fe47d599f45ca5e042082ac6952972604030
Amanda Faro <amanda.faro@dp6.com.br>
2020-09-28 12:05:06 -0300

    Alteração nas regras do GruntJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,79 @@
 /*global module:false*/
-module.exports = grunt => {
+module.exports = (grunt) => {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %> */\n',
+    banner:
+      '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */\n',
     replace: {
       compile: {
-        src: 'src/panel.html',
-        dest: 'dist/panel.html',
-        replacements: [{
-          from: /<!-- @dev-css [\s\S]*? dev-css@ -->/img,
-          to: '<link rel="stylesheet" type="text/css" href="css/all.css"/>'
-        }, {
-          from: /<!-- @dev-js [\s\S]*? dev-js@ -->/img,
-          to: '<script src="js/jquery.js"></script><script src="js/all.js"></script>'
-        }]
-      }
+        src: 'src/*.html',
+        dest: 'dist/',
+        replacements: [
+          {
+            from: /<!-- @dev-css [\s\S]*? dev-css@ -->/gim,
+            to: '<link rel="stylesheet" type="text/css" href="css/all.css"/>',
+          },
+          {
+            from: /<!-- @dev-js [\s\S]*? dev-js@ -->/gim,
+            to: '<script src="js/jquery.js"></script><script src="js/all.js"></script>',
+          },
+        ],
+      },
     },
     cssmin: {
       combine: {
         files: {
-          'dist/css/all.css': ['src/css/normalize.css', 'src/css/style.css']
-        }
-      }
+          'dist/css/all.css': ['src/css/normalize.css', 'src/css/style.css'],
+        },
+      },
     },
     concat: {
       options: {
         banner: '<%= banner %>',
-        stripBanners: true
+        stripBanners: true,
       },
       dist: {
         files: {
-          'dist/js/all.js': ['src/js/rules.js', 'src/js/metadata.js', 'src/js/script.js', 'src/js/eventos.js', 'src/js/tagueamento.js'],
-          'dist/js/devtools.js': 'src/js/devtools.js'
-        }
-      }
+          'dist/js/all.js': [
+            'src/js/rules.js',
+            'src/js/metadata.js',
+            'src/js/eventos.js',
+            'src/js/tagueamento.js',
+            'src/js/jquery.js',
+          ],
+          'dist/js/script.js': 'src/js/script.js',
+          'dist/js/devtools.js': 'src/js/devtools.js',
+          'dist/bowserFiles/js/index.js': 'src/bowserFiles/js/index.js',
+          'dist/bowserFiles/js/ludwig.js': 'src/bowserFiles/js/ludwig.js',
+          'dist/bowserFiles/lib/ajv.js': 'src/bowserFiles/lib/ajv.js',
+          'dist/bowserFiles/js/ajv.min.js': 'src/bowserFiles/js/ajv.min.js',
+          'dist/bowserFiles/lib/schema_parser.js': 'src/bowserFiles/lib/schema_parser.js',
+        },
+      },
     },
     jshint: {
       options: grunt.file.readJSON('jshint.json'),
       gruntfile: {
-        src: 'Gruntfile.js'
+        src: 'Gruntfile.js',
       },
       js: {
-        src: ['src/js/script.js', 'src/js/eventos.js', 'src/js/tagueamento.js']
-      }
+        src: ['src/js/script.js', 'src/js/eventos.js', 'src/js/tagueamento.js'],
+      },
     },
     copy: {
       compile: {
-        files: [{
-          cwd: 'src/',
-          expand: true,
-          src: ['manifest.json', 'icons/*', 'img/*', 'js/jquery.js', 'devtools.html'],
-          dest: 'dist/'
-        }]
-      }
+        files: [
+          {
+            cwd: 'src/',
+            expand: true,
+            src: ['manifest.json', 'icons/*', 'img/*', 'js/jquery.js', 'devtools.html'],
+            dest: 'dist/',
+          },
+        ],
+      },
     },
-    clean: ['dist']
+    clean: ['dist'],
   });
 
   // These plugins provide necessary tasks.
@@ -72,5 +89,4 @@ module.exports = grunt => {
   grunt.registerTask('css', ['cssmin']);
   grunt.registerTask('js', ['concat', 'jshint']);
   grunt.registerTask('default', ['clean', 'html', 'css', 'js', 'copy']);
-
 };
