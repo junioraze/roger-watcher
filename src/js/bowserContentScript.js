9d84f2c67d6d84af0b347ca3616e26a4f3826b8c
Gabriel Tellaroli Ramos <gabriel.tellaroli@dp6inc.local>
2020-11-25 15:35:05 -0300

    refactor(bowserContentScript.js): Passei todo o codigo da contentScript para dentro do if que verifica se o push_c já existe.

--- src/js/bowserContentScript.js
+++ src/js/bowserContentScript.js
@@ -1,51 +1,49 @@
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message == "inject_bowser_script") {
-        var script = document.createElement("script");
-        script.innerHTML = `
-        window["${request.datalayer}"].forEach(elem=>{
-          window.postMessage({
-            dataLayer:"dispatch_datalayer_object_from_window",
-            datalayer_object:JSON.parse(JSON.stringify(elem)),
-            url: window.origin
-          },"*");
-        });
-  
-  
+    var script = document.createElement("script");
+    script.innerHTML = ` 
         if (!window["${request.datalayer}"].push_c) {
+          window["${request.datalayer}"].forEach(elem=>{
+            window.postMessage({
+              dataLayer:"dispatch_datalayer_object_from_window",
+              datalayer_object:JSON.parse(JSON.stringify(elem)),
+              url: window.origin
+            },"*");
+          });
           console.log("DataLayer Name sent from Background Script: ${request.datalayer}");
           window["${request.datalayer}"].push_c = window["${request.datalayer}"].push;
           window["${request.datalayer}"].push = function (obj) {
             window["${request.datalayer}"].push_c(obj);
             window.postMessage({
               dataLayer:"dispatch_datalayer_object_from_window",
-              datalayer_object:obj
+              datalayer_object:obj,
+              url: window.origin
             },"*");
           }
       };`;
-        document.head.appendChild(script);
-      }
-      sendResponse({
-        message: "script_injected_successfully",
-      });
- 
+    document.head.appendChild(script);
+  }
+  sendResponse({
+    message: "script_injected_successfully",
   });
 
-  window.addEventListener("message", function (event) {
-    if (
-      event.data.dataLayer &&
-      event.data.dataLayer == "dispatch_datalayer_object_from_window"
-    ) {
-      console.log(
-        "Content script received message: " +
-          JSON.stringify(event.data.datalayer_object)
-      );
-  
-      chrome.runtime.sendMessage("oigelofjcjfbegbeckbpjglkkmnmibbo",{
-        message: "accepted",
-        datalayer_object: event.data.datalayer_object,
-        url: event.data.url,
-      },function(response){});
-    }
-  });
-  
+});
+
+window.addEventListener("message", function (event) {
+  if (
+    event.data.dataLayer &&
+    event.data.dataLayer == "dispatch_datalayer_object_from_window"
+  ) {
+    console.log(
+      "Content script received message: " +
+      JSON.stringify(event.data.datalayer_object)
+    );
+
+    chrome.runtime.sendMessage("oigelofjcjfbegbeckbpjglkkmnmibbo", {
+      message: "accepted",
+      datalayer_object: event.data.datalayer_object,
+      url: event.data.url,
+    }, function (response) { });
+  }
+});
